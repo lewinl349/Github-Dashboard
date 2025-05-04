89b71dc6feba096ddbbb178d32357c47bbdbd776
lewinl349 <lewinl349@gmail.com>
2025-05-04 00:50:33 -0400

    Import Electron Forge + React

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// const root = createRoot(document.body);
+// root.render(<h2>Hello from React!</h2>);
+
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
@@ -5,7 +11,6 @@ const createWindow = () => {
     width: 800,
     height: 600
   })
-
   win.loadFile('index.html')
 }
 
